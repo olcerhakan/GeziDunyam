8596dfb415b3e27564c7b0f7cf48238d6d0d63fe
hakan ölçer <ha_-kan-_olcer95@windowslive.com>
2020-06-26 16:22:10 +0300

    Post & Categories & BaseController & CategoriesPartial

---
+++ GeziDunyam/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using GeziDunyam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeziDunyam.Controllers
+{
+    public class BaseController : Controller
+    {
+
+        protected ApplicationDbContext db = new ApplicationDbContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ GeziDunyam/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GeziDunyam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeziDunyam.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public List<Post> Posts { get; set; }
+    }
+}
