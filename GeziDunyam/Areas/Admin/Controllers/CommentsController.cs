5d0fbeb8e2d00c58aabe12103a4a9772c9173361
hakan ölçer <ha_-kan-_olcer95@windowslive.com>
2020-06-28 20:55:54 +0300

    Approve Comment & Comment Controller

---
+++ GeziDunyam/Areas/Admin/Controllers/CommentsController.cs
@@ -0,0 +1,28 @@
+﻿using GeziDunyam.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeziDunyam.Areas.Admin.Controllers
+{
+    public class CommentsController : AdminBaseController
+    { 
+        // GET: Admin/Comments
+        public ActionResult Index()
+        {
+            return View(db.Comments.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult ChangeState(int id, bool isPublished)
+        {
+            var comment = db.Comments.Find(id);
+            comment.State = isPublished ? CommentState.Approved : CommentState.Rejected;
+            db.SaveChanges();
+
+            return new HttpStatusCodeResult(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
